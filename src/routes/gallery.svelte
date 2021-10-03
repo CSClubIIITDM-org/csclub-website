fb3c88025700c2715f25757add98399aef74723f
Puru Vijay <devpuruvj@gmail.com>
2021-10-04 01:06:14 +0530

    Initial Commit

---
+++ src/routes/gallery.svelte
@@ -0,0 +1,15 @@
+<h1>Gallery</h1>
+
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, iure. Enim, est libero iusto
+  dolores incidunt ad consectetur reiciendis perferendis maxime obcaecati, deserunt vero. Ipsam
+  maiores corrupti asperiores soluta. Corrupti.
+</p>
+
+Hello
+
+<style>
+  h1 {
+    font-size: 40px;
+  }
+</style>
