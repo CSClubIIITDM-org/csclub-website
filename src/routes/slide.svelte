2dacd14f20e1bf7053e1fcd7e6aa6e6b3afe062b
V Nagasai <83576465+NSVEGUR@users.noreply.github.com>
2021-10-23 13:49:38 +0530

    first commit (#10)
    * first commit
    
    * second commit
    
    * second commit

---
+++ src/routes/slide.svelte
@@ -0,0 +1,46 @@
+<script></script>
+
+<div class="slide" style="transform: translateX(0%)">
+	<h1>CS Club IIITDM</h1>
+</div>
+<div class="slide" style="transform: translateX(100%)">
+	<h1>Gallery</h1>
+</div>
+<div class="slide" style="transform: translateX(200%)">
+	<h1>Competative Programming</h1>
+</div>
+<div class="slide" style="transform: translateX(300%)">
+	<h1>Project, Research & Industrial Talks</h1>
+</div>
+<div class="slide" style="transform: translateX(400%)">
+	<h1>Educating Information Technology and Hardware</h1>
+</div>
+<div class="slide" style="transform: translateX(500%)">
+	<h1>Networking</h1>
+</div>
+<div class="slide" style="transform: translateX(600%)">
+	<h1>People</h1>
+</div>
+<div class="slide" style="transform: translateX(700%)">
+	<h1>Timeline</h1>
+</div>
+<div class="dots" />
+
+<style>
+	.slide {
+		position: absolute;
+		top: 0;
+		width: 100%;
+		height: 100%;
+		margin: auto auto;
+		display: grid;
+		direction: row;
+		grid-template-columns: auto;
+		grid-template-rows: auto auto auto;
+		justify-items: center;
+		align-content: center;
+		justify-content: center;
+		/* THIS creates the animation! */
+		transition: transform ease-in-out 2s;
+	}
+</style>
