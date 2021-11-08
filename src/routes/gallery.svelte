104da3d9fc513fc04669e7f5f7c8a8bf1d971735
Abhi-1001 <61421046+Abhi-1001@users.noreply.github.com>
2021-11-08 19:42:16 +0530

    Gallery Updates (#14)

--- src/routes/gallery.svelte
+++ src/routes/gallery.svelte
@@ -17,262 +17,203 @@
     const next = () => {
       index = (index + 1) % carouselPhotos.length;
     };
-  </script>
+</script>
   
-  <body height="auto">
-    <header style="background-color:rgb(31, 41, 55);">
-      <div>
-        <nav>
-          <input type="checkbox" id="check" />
-          <label for="check" class="checkbtn "><i class="fas fa-bars" />☰</label>
-          <img src="" width="70" height="70" alt="" />
-          <span class="vl" />
-          <img src="" width="70" height="70" alt="" />
-          <a href="." class="title"><strong>&emsp &emsp CS CLUB</strong></a>
-          <ul>
-            <li><a href=".">&emsp &emsp Home &emsp &emsp</a></li>
-            <li><a href="gallery">&emsp &emsp Gallery &emsp &emsp</a></li>
-            <li><a href="cp">&emsp &emsp CP &emsp &emsp</a></li>
-            <li><a href="prit">&emsp &emsp PRIT &emsp &emsp</a></li>
-            <li><a href="edith">&emsp &emsp EDITH &emsp &emsp</a></li>
-            <li><a href="networking"> &emsp &emsp Networking &emsp &emsp</a></li>
-            <li><a href="people">&emsp &emsp People &emsp &emsp</a></li>
-            <li><a href="timeline">&emsp &emsp Timeline &emsp &emsp</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  
-    <br /><br />
+<body>  
     <div class="heading">
       <h1>Gallery</h1>
     </div>
-  
-    <div><h2>&emsp CS club 2019 &emsp</h2></div>
-    <div style="text-align: center;">
-    <img
-      padding="50px"
-      src="https://bit.ly/fcc-relaxing-cat"
-      alt="Add pic here"
-      style="
-              border-radius: 10%;
-              height:20%;
-              width:35%;"
-    /></div>
-    <div><h2>&emsp Vashisht 2019 &emsp</h2></div>
-    <div style="text-align: center;">
-    <img
-      padding="50px"
-      src="https://bit.ly/fcc-relaxing-cat"
-      alt="Add pic here"
-      style="
-              border-radius: 10%;
-              height:20%;
-              width:35%;"
-    /></div>
-    <div><h2>&emsp random sec &emsp</h2></div>
-    <div style="text-align: center;">
-    <img
-      padding="50px"
-      src="https://bit.ly/fcc-relaxing-cat"
-      alt="Add pic here"
-      style="
-              border-radius: 10%;
-              height:20%;
-              width:35%;"
-    /></div>
-    <br /><br />
+		
+		<div class="gallery-tiles">
+			<div class="tiles">
+				<div>
+					<h2>CS Club 2019</h2>
+				</div>
+				<div class="box">
+
+				</div>
+			</div>
+
+			<div class="tiles">
+				<div>
+					<h2>Vashisht 2019</h2>
+				</div>
+				<div class="box">
+
+				</div>
+			</div>
+
+			<div class="tiles">
+				<div>
+					<h2>Random Sec</h2>
+				</div>
+				<div class="box">
+
+				</div>
+			</div>
+		</div>
+
     <div id="main" class="main">
-      <div id="first" class="first">
-        <div style="font-size:2.0rem">Location</div>
-        <div class="mapouter">
-          <div class="gmap_canvas">
-            <iframe
-              id="gmap_canvas"
-              src="https://maps.google.com/maps?q=iiitdmk&t=k&z=15&ie=UTF8&iwloc=&output=embed"
-              frameborder="0"
-              scrolling="no"
-              marginheight="0"
-              marginwidth="0"
-            /><a href="https://www.whatismyip-address.com/divi-discount/" /><br
-            /><style>
-              .mapouter {
-                position: relative;
-                text-align: right;
-                height: auto;
-                width: auto;
-              }
-            </style><a href="https://www.embedgooglemap.net"
-              >embed google maps iframe</a
-            ><style>
-              .gmap_canvas {
-                overflow: hidden;
-                background: none !important;
-                height: auto;
-                width: auto;
-              }
-            </style>
-          </div>
-        </div>
-        <div style="font-size:2.0rem">Contact Us:</div>
-        <div style="font-size:2.2rem">CS Club</div>
-        <div>
-          <p>IIITDM Kancheepuram,</p>
-          <p>Chennai : 600127.</p>
-        </div>
-        <div>
-          Email: <a href="mailto: csclub@iiitdm.ac.in">csclub@iiitdm.ac.in</a>
-        </div>
-      </div>
+      	<div id="row" class="row">
+			<div class="column">
+				<div style="font-size:2.0rem">Location</div>
+				<div class="mapouter">
+					<div class="gmap_canvas">
+						<!-- svelte-ignore a11y-missing-attribute -->
+						<iframe
+							id="gmap_canvas"
+							src="https://maps.google.com/maps?q=iiitdmk&t=k&z=15&ie=UTF8&iwloc=&output=embed"
+							frameborder="0"
+							scrolling="no"
+							marginheight="0"
+							marginwidth="0"/>
+						<!-- svelte-ignore a11y-missing-content -->
+						<a href="https://www.whatismyip-address.com/divi-discount/"/>
+						<br/>
+						<style>
+						.mapouter {
+							position: relative;
+							text-align: left;
+							height: auto;
+							width: auto;
+						}
+						</style>
+						<!-- svelte-ignore a11y-missing-content -->
+						<a href="https://www.embedgooglemap.net"></a>
+						<style>
+						.gmap_canvas {
+							overflow: hidden;
+							background: none !important;
+							height: auto;
+							width: auto;
+						}
+						</style>
+					</div>
+				</div>
+			</div>
+			<div class="column">
+				<div style="font-size:2.0rem">Contact Us:</div>
+				<div style="font-size:2.2rem">CS Club</div>
+				<div>
+					<p>IIITDM Kancheepuram,</p>
+					<p>Chennai : 600127.</p>
+				</div>
+				<div>
+					Email: <a href="mailto: csclub@iiitdm.ac.in">csclub@iiitdm.ac.in</a>
+				</div>
+			</div>
+		</div>
+		
+		<p class="copyright">
+			Copyrights © 2020 | All Rights Reserved | Done by
+			<span>Web Team</span>
+		</p>
     </div>
-    <p class="copyright">
-      Copyrights © 2020 | All Rights Reserved | Done by
-      <span>Web Team</span>
-    </p>
-  </body>
+</body>
+
   
-  <section />
-  
-  <style>
+<style>
     body {
-      background-color: #101827;
-      height: auto;
+      background-color: var(--app-color-shell);
+      margin: 0;
     }
     p {
       color: whitesmoke;
     }
-    nav {
-      height: 100px;
-      width: 100%;
-      position: fixed;
-      left: 0;
-      background: rgb(31, 41, 55);
-      z-index: 100;
-    }
-    nav img {
-      margin-top: 10px;
-    }
-    * {
-      padding: 0;
-      margin: 0;
-      box-sizing: border-box;
-      font-family: "Poppins", sans-serif;
-      color: #3bdf8a;
-    }
-    .title {
-      color: #3bdf8a;
-      text-decoration: none;
-      font-size: 35px;
-      line-height: 80px;
-      font-weight: bold;
-    }
-    nav ul {
-      float: right;
-      margin-right: 20px;
-    }
-    nav ul li {
-      display: inline-block;
-      line-height: 80px;
-      margin: 0 5px;
-    }
-    nav ul li a:hover {
-      color: white;
-    }
-    .checkbtn {
-      font-size: 30px;
-      float: right;
-      margin-right: 40px;
-      line-height: 80px;
-      display: none;
-    }
-    #check {
-      display: none;
-    }
-    section {
-      position: absolute;
-      top: 120px;
-    }
-    @media (max-width: 920px) {
-      .checkbtn {
-        display: block;
-      }
-      ul {
-        position: fixed;
-        width: 100%;
-        height: 100vh;
-        top: 80px;
-        left: -100%;
-        background: rgb(31, 41, 55);
-        text-align: center;
-        transition: all 0.5s;
-      }
-      nav ul li {
-        display: block;
-        font-size: 20px;
-      }
-      #check:checked ~ ul {
-        left: 0;
-      }
-    }
-  
     .heading {
-      background: black;
-      color: #34d399;
-      text-align: center;
-      font-size: 2.5rem;
-      padding: 170px 250px;
+      align-items: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+	
+      height: 100vh;
+      width: 100%;
       margin: 0;
+
+      background: black center center / cover no-repeat;
     }
-  
-    h2 {
-      position: relative;
-      font-size: 60px;
-      z-index: 1;
+
+    .heading h1 {
+      text-align: center;
+      font-size: 150px;
+	  font-family: var(--app-heading-font-family);
+	  font-style: normal;
+    }
+
+    .gallery-tiles {
+      padding-top: 5%;
+      padding-left: 5%;
+      padding-right: 5%;
+      width: 100%;
+    }
+
+    .gallery-tiles h2 {
+      font-size: 40px;
       overflow: hidden;
       text-align: center;
     }
-    h2:before,
+    .gallery-tiles h2:before,
     h2:after {
-      position: absolute;
-      top: 51%;
-      overflow: hidden;
+      background-color: var(--app-color-primary);
+      content: "";
+      display: inline-block;
+      height: 2px;
+      position: relative;
+      vertical-align: middle;
       width: 50%;
-      height: 1px;
-      content: "\a0";
-      background-color: #34d399;
     }
-    h2:before {
+    .gallery-tiles h2:before {
+      right: 0.5em;
       margin-left: -50%;
-      text-align: right;
     }
+    .gallery-tiles h2:after {
+      left: 0.5em;
+      margin-right: -50%;
+    }
+
+    .tiles {
+      padding-bottom: 5%;
+    }
+
+	.tiles h2 {
+		font-family: var(--app-main-font-family);
+		font-style: normal;
+	}
+
+	.box {
+	background-color: var(--app-color-primary);
+	box-sizing: border-box;
+	padding: 5%;
+	margin-top: 1%;
+	height: 300px;
+	}
   
     .copyright {
       color: gray;
       text-align: center;
     }
   
-    .first {
+    .row {
       display: flex;
-      flex-direction: column;
-      align-items: flex-start;
       justify-content: center;
-      padding: 50px;
-      margin: 10px;
-      color: white;
+      padding: 15px;
     }
   
-    .first p {
+    .row p {
       letter-spacing: 0.5px;
       font-size: 1.25rem;
       margin-bottom: 10px;
     }
-    .main {
-      margin: 35px;
-      background: black;
-      width: auto;
-      height: auto;
-      border-radius: 16px;
-      flex-direction: centre;
+
+    .column {
+      flex: 50%;
+      padding: 25px;
     }
-  </style>
-  
+
+    .main {
+      background: black;
+      width: 100%;
+      height: auto;
+      border-radius: 25px;
+    }
+</style>
