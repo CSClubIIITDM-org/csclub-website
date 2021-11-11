82d2ed8b5aefccdf264fdd5d4814c669f5163af6
unknown <abhi10012001@gmail.com>
2021-11-11 21:54:14 +0530

    People page

---
+++ src/routes/people.svelte
@@ -0,0 +1,363 @@
+<body>
+	<div class="heading">
+		<h1>People</h1>
+	</div>
+	
+	<div class="subheading">
+		<h2>Members (2020 - 2021)</h2>
+	</div>
+	
+	<h3 class="title">Faculty Advisor</h3>
+	<hr class="line">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+
+	<h3 class="title">Head Core</h3>
+	<hr class="line">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+
+	<h3 class="title">Core Team</h3>
+	<hr class="line">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Northern Lights" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Northern Lights" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+
+	<h3 class="title">Joint Cores</h3>
+	<hr class="line">
+
+	<h3 class="subtitle">CP</h3>
+	<hr class="subline">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+
+	<h3 class="subtitle">Edith</h3>
+	<hr class="subline">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+	
+	<h3 class="subtitle">Networking</h3>
+	<hr class="subline">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+
+	<h3 class="subtitle">PRIT</h3>
+	<hr class="subline">
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Cinque Terre" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+		
+		<div class="responsive">
+			<div class="gallery">
+				<a target="_blank" href="/logo.png">
+				<img src="/logo.png" alt="Forest" width="600" height="400">
+				</a>
+				<div class="desc">Add a description of the image here</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+<style>
+	.heading {
+	  align-items: center;
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: center;
+	
+	  height: 100vh;
+	  width: 100%;
+	  margin: 0;
+
+	  background: black center center / cover no-repeat;
+	}
+
+	.heading h1 {
+	  text-align: center;
+	  font-size: 150px;
+	  font-family: var(--app-heading-font-family);
+	  font-style: normal;
+	}
+
+	.subheading {
+		padding-top: 1%;
+		width: 100%;
+		margin: 0;
+		text-align: center;
+	}
+
+	.subheading h2 {
+		color: white;
+	}
+
+	.title {
+		padding-top: 5%;
+		font-size: 40px;
+	}
+
+	.line {
+		display: block;
+		width: 20%;
+		border: 2px solid var(--app-color-primary);
+		border-style: inset;
+	}
+
+	.subtitle {
+		padding-top: 2%;
+		font-size: 30px;
+	}
+
+	.subline {
+		display: block;
+		width: 5%;
+		border: 1.5px solid var(--app-color-primary);
+		border-style: inset;
+	}
+
+	.container {
+		box-sizing: border-box;
+		margin: 1%;
+		display: flex;
+		justify-content: center;
+	}
+
+	div.gallery {
+		border: 1px solid #ccc;
+		border-radius: 5%;
+	}
+
+	div.gallery:hover {
+		border: 1px solid #777;
+	}
+
+	div.gallery img {
+		width: 100%;
+		height: auto;
+	}
+
+	div.desc {
+		padding: 15px;
+		text-align: center;
+	}
+
+	.responsive {
+		padding: 0 6px;
+		float: left;
+		width: auto;
+	}
+
+	@media only screen and (max-width: 700px) {
+	.responsive {
+		width: 50%;
+		margin: 6px 0;
+	}
+	}
+
+	@media only screen and (max-width: 500px) {
+		.responsive {
+			width: 100%;
+		}
+	}
+</style>
