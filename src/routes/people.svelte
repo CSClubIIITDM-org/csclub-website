60622b3761ddea00d2116163aa72205b46bd2c8a
Puru Vijay <awesomepuruvj@gmail.com>
2022-01-21 16:08:27 +0530

    People page

--- src/routes/people.svelte
+++ src/routes/people.svelte
@@ -3,10 +3,113 @@
 
 	let isLazysizesImported = false;
 
+	const people = {
+		"Faculty Advisor": [
+			{
+				name: "Dr. Masilamani V",
+				photo: "masilamani",
+			},
+		],
+		"Head Core": [
+			{
+				name: "Subash Mylraj",
+				photo: "subash",
+			},
+		],
+		"WebOps Head": [
+			{
+				name: "Puru Vijayvargia",
+				photo: "puru-vijayvargia",
+			},
+		],
+		"Core Team": [
+			{
+				name: "Ankit Kumar",
+				photo: "ankit-kumar-bharti",
+			},
+			{
+				name: "Atharva Kadlag",
+				photo: "atharva-kadlag",
+			},
+			{
+				name: "G V Sanjay Reddy",
+				photo: "sanjay-reddy",
+			},
+			{
+				name: "Sreenivas Dheeraj",
+				photo: "sreenivas-dheeraj",
+			},
+			{
+				name: "Sumit Kumar",
+				photo: "sumit-kumar",
+			},
+		],
+		"Competetive Programming": [
+			{
+				name: "Jashwanth",
+				photo: "jaswanth-peddisetti",
+			},
+			{
+				name: "Jitin",
+				photo: "chekka-jitin-krishna",
+			},
+			{
+				name: "Somashekhar",
+				photo: "somashekhar",
+			},
+			{
+				name: "R Shyam Sundar",
+				photo: "shyam-sundar",
+			},
+			{
+				name: "Rohail Alam",
+				photo: "rohail-alam",
+			},
+		],
+		EDiTH: [
+			{
+				name: "Jagadeeshan S",
+				photo: "jagadeeshan",
+			},
+			{
+				name: "Mariyam Joory",
+				photo: "mariyam",
+			},
+			{
+				name: "Nithish Vasanth",
+				photo: "nithish-vasanth",
+			},
+			{
+				name: "Yerramsetti V S N Sai Mohith",
+				photo: "mohith",
+			},
+		],
+		Networking: [
+			{
+				name: "Dheeraj Kumar",
+				photo: "dheeraj-kumar",
+			},
+			{
+				name: "Khushal Gautam",
+				photo: "khushal-gautam",
+			},
+		],
+		PRIT: [
+			{
+				name: "Himanshu Malviya",
+				photo: "himanshu",
+			},
+			{
+				name: "Yash Agarwal",
+				photo: "yash-agarwal",
+			},
+		],
+	};
+
 	/**
 	 * @param {HTMLImageElement} node
 	 */
-	export function lazy(node) {
+	function lazy(node) {
 		const srcVal = node.src;
 		node.src = "";
 		node.dataset.src = srcVal;
@@ -20,339 +123,36 @@
 	}
 </script>
 
+<svelte:head>
+	<title>People / CS Club / IIITDM Kancheepuram</title>
+</svelte:head>
+
 <InitialScreen>People</InitialScreen>
+
 <main class="center">
-	<!--  -->
-	<!-- <section></section> -->
-	<section id="faculty-advisor">
-		<h2>Faculty Advisor</h2>
-		<br />
+	{#each Object.entries(people) as [role, person]}
+		<section class="people-container">
+			<h2>{role}</h2>
 
-		<span class="person">
-			<div class="picture">
-				<!-- <img use:lazy src="/people/.webp" alt="PLACEHOLDER" /> -->
-			</div>
-			<br />
-			PLACEHOLDER
-		</span>
-	</section>
-
-	<br /><br />
-
-	<section id="head-core">
-		<h2>Head Core</h2>
-		<br />
-
-		<span class="person">
-			<div class="picture">
-				<img draggable="false" alt="" />
-				<img src="/people/subash.webp" alt="Subash Mylraj" use:lazy />
-			</div>
-			<br />
-			Subash Mylraj
-		</span>
-	</section>
-	<br /><br />
-	<section id="head-core">
-		<h2>WebOps Head</h2>
-		<br />
-
-		<span class="person">
-			<div class="picture">
-				<img
-					src="/people/puru-vijayvargia.webp"
-					alt="Puru Vijayvargia"
-					use:lazy
-				/>
-			</div>
-			<br />
-			Puru Vijayvargiya
-		</span>
-	</section>
-
-	<br /><br />
-
-	<section id="core-team">
-		<h2>Core Team</h2>
-
-		<br />
-
-		<div class="flex">
-			<span class="person">
-				<div class="picture">
-					<img
-						src="/people/ankit-kumar-bharti.webp"
-						alt="Ankit Kumar"
-						use:lazy
-					/>
-				</div>
-				<br />
-				Ankit Kumar
-			</span>
-
-			<span class="spacer" />
-
-			<span class="person">
-				<div class="picture">
-					<img
-						src="/people/atharva-kadlag.webp"
-						alt="Atharva Kadlag"
-						use:lazy
-					/>
-				</div>
-				<br />
-				Atharva Kadlag
-			</span>
-
-			<span class="spacer" />
-
-			<span class="person">
-				<div class="picture">
-					<img src="/people/sanjay-reddy.webp" alt="Sanjay Reddy" use:lazy />
-				</div>
-				<br />
-				G V Sanjay Reddy
-			</span>
-		</div>
-		<br /><br />
-		<div class="flex">
-			<span class="person">
-				<div class="picture">
-					<img
-						src="/people/sreenivas-dheeraj.webp"
-						alt="Sreenivas Dheeraj"
-						use:lazy
-					/>
-				</div>
-				<br />
-				Sreenivas Dheeraj
-			</span>
-
-			<span class="spacer" />
-
-			<span class="person">
-				<div class="picture">
-					<img src="/people/sumit-kumar.webp" alt="Sumit Kumar" use:lazy />
-				</div>
-				<br />
-				Sumit Kumar
-			</span>
-		</div>
-	</section>
-	<br /><br />
-	<section id="joint-core">
-		<h2>Joint Core</h2>
-		<br />
-		<section id="cp">
-			<h2>CP</h2>
-			<br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<img
-							src="/people/jaswanth-peddisetti.webp"
-							alt="Jashwanth"
-							use:lazy
-						/>
-					</div>
-					<br />
-					Jashwanth
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<img
-							src="/people/chekka-jitin-krishna.webp"
-							alt="Chekka Jitin Krishna"
-							use:lazy
-						/>
-					</div>
-					<br />
-					Jitin
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<img src="/people/shyam-sundar.webp" alt="Shyam Sundar" use:lazy />
-					</div>
-					<br />
-					R Shyam Sundar
-				</span>
-			</div>
 			<br /><br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<img src="/people/rohail-alam.webp" alt="Rohail alam" use:lazy />
-					</div>
-					<br />
-					Rohail Alam
-				</span>
 
-				<span class="spacer" />
+			<div class="grid">
+				{#each person as { name, photo }}
+					<div class="person">
+						<div class="picture">
+							<img src="/people/{photo}.webp" alt={name} use:lazy />
+						</div>
 
-				<span class="person">
-					<div class="picture">
-						<img src="/people/somashekhar.webp" alt="Somashekhar" use:lazy />
+						<br />
+
+						{name}
 					</div>
-					<br />
-					Somashekhar
-				</span>
+				{/each}
 			</div>
 		</section>
-		<br /><br />
-		<section id="edith">
-			<h2>Edith</h2>
-			<br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<img src="/people/jagadeeshan.webp" alt="Jagadeeshan" use:lazy />
-					</div>
-					<br />
-					Jagadeeshan S
-				</span>
+		<br /><br /><br /><br />
+	{/each}
 
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<img src="/people/mariyam.webp" alt="Mariyam" use:lazy />
-					</div>
-					<br />
-					Mariyam Joory
-				</span>
-			</div>
-			<br /><br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<img
-							src="/people/nithish-vasanth.webp"
-							alt="Nithish Vasanth"
-							use:lazy
-						/>
-					</div>
-					<br />
-					Nithish Vasanth
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<img src="/people/mohith.webp" alt="Mohith" use:lazy />
-					</div>
-					<br />
-					Yerramsetti V S N Sai Mohith
-				</span>
-			</div>
-		</section>
-		<br /><br />
-		<section id="networking">
-			<h2>Networking</h2>
-			<br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<img
-							src="/people/dheeraj-kumar.webp"
-							alt="Dheeraj Kumar"
-							use:lazy
-						/>
-					</div>
-					<br />
-					Dheeraj Kumar
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<img
-							src="/people/khushal-gautam.webp"
-							alt="Khushal Gautam"
-							use:lazy
-						/>
-					</div>
-					<br />
-					Khushal Gautam
-				</span>
-			</div>
-		</section>
-		<br /><br />
-		<section id="prit">
-			<h2>PRIT</h2>
-			<br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<img src="/people/himanshu.webp" alt="Himanshu" use:lazy />
-					</div>
-					<br />
-					Himanshu Malviya
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<img src="/people/yash-agarwal.webp" alt="Yash Agarwal" use:lazy />
-					</div>
-					<br />
-					Yash Agrawal
-				</span>
-			</div>
-		</section>
-		<br /><br />
-		<section id="web-development-team">
-			<h2>Web Development Team</h2>
-			<br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<!-- <img draggable="false" src="/people/.webp" alt="PLACEHOLDER" /> -->
-					</div>
-					<br />
-					PLACEHOLDER
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<!-- <img draggable="false" src="/people/.webp" alt="PLACEHOLDER" /> -->
-					</div>
-					<br />
-					PLACEHOLDER
-				</span>
-			</div>
-			<br /><br />
-			<div class="flex">
-				<span class="person">
-					<div class="picture">
-						<!-- <img draggable="false" src="/people/.webp" alt="PLACEHOLDER" /> -->
-					</div>
-					<br />
-					PLACEHOLDER
-				</span>
-
-				<span class="spacer" />
-
-				<span class="person">
-					<div class="picture">
-						<!-- <img draggable="false" src="/people/.webp" alt="PLACEHOLDER" /> -->
-					</div>
-					<br />
-					PLACEHOLDER
-				</span>
-			</div>
-		</section>
-	</section>
 	<br /><br />
 </main>
 
@@ -363,10 +163,20 @@
 		padding: 0.25rem 2rem;
 	}
 
-	.flex {
-		display: inline-flex;
-		flex-wrap: wrap;
+	.people-container {
+		width: 100%;
+
+		display: grid;
+		place-items: center;
+	}
+
+	.grid {
+		width: 100%;
+
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(15rem, 15rem));
 		gap: 1rem;
+		justify-content: center;
 	}
 
 	.center {
@@ -375,6 +185,11 @@
 		align-items: center;
 		text-align: center;
 		width: 100%;
+	}
+
+	.person {
+		display: grid;
+		place-items: center;
 	}
 
 	.picture {
@@ -405,10 +220,6 @@
 	@include small-screens {
 		main {
 			padding: 0.25vw 2vw;
-		}
-
-		.flex {
-			flex-direction: column;
 		}
 
 		h2 {
